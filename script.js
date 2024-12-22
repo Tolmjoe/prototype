aee8c139c1812a558ca83fc461cae1d21d6c0de4
iamfemiab <abitogun22@gmail.com>
2024-12-22 18:19:40 +0100

    integrated bounicing effect and arranged css files into folder

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 
-
-
-
-
 $(window).on("load", function () {
     $("profile_inner").hide();
     $("#hi ").hide();
-    $("#mynameis").hide();
     $("#femi").hide();
 
-    $("#phone ").hide();
-    $("#mail2 ").hide();
-    $("#title ").hide();
+   
 
     $("#hi").toggle(1000, function () {
         $(".profile_inner").show();
        
-        $("#mynameis").toggle(1000, function () {
-
             $("#femi").toggle(1000, function () {
 
                 setTimeout(() => {
@@ -25,41 +16,15 @@ $(window).on("load", function () {
                     callnext();
                 }, 100)
             })
-
-        })
-        
+ 
     })
-
-
-
-
-    function callnext() {
-
-        $("#title ").toggle(1000, function () {
-
-            $("#phone ").toggle(1000, function () {
-
-                $("#mail2 ").toggle(1000)
-
-            })
-        })
-
-
-    }
 
 
 
     setInterval(() => {
         $(".profile #hi").effect("bounce", { times: 8, distance: -60 }, 1000);
-        $(".profile #mynameis").effect("bounce", { times: 8, distance: -50 }, 1000);
         $(".profile  #femi").effect("bounce", { times: 10, distance: -20 }, 1000);
     }, 3000)
-
-
-
-
-
-
 
 }
 
