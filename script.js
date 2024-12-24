011972e7f6dd3fb3ab591b9a07f059200eff4e0e
Tolmjoe <jayeobae@gmail.com>
2024-12-24 18:29:34 +0100

    background/prototype-logo at the footer/overall design/toggle theme/reponsive background/tata

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-
 $(window).on("load", function () {
-    $("profile_inner").hide();
-    $("#hi ").hide();
-    $("#femi").hide();
+  $("profile_inner").hide();
+  $("#hi ").hide();
+  $("#femi").hide();
 
-   
+  $("#hi").toggle(1000, function () {
+    $(".profile_inner").show();
 
-    $("#hi").toggle(1000, function () {
-        $(".profile_inner").show();
-       
-            $("#femi").toggle(1000, function () {
+    $("#femi").toggle(1000, function () {
+      setTimeout(() => {
+        callnext();
+      }, 100);
+    });
+  });
 
-                setTimeout(() => {
+  setInterval(() => {
+    $(".profile #hi").effect("bounce", { times: 8, distance: -60 }, 1000);
+    $(".profile  #femi").effect("bounce", { times: 10, distance: -20 }, 1000);
+  }, 3000);
+});
 
-                    callnext();
-                }, 100)
-            })
- 
-    })
+$(document).ready(() => {
+  $("#dkmodebutton").click(() => {
+    $(".body1, .body2, .body3, .foot, .welcomemessage").toggleClass("dkmode");
 
-
-
-    setInterval(() => {
-        $(".profile #hi").effect("bounce", { times: 8, distance: -60 }, 1000);
-        $(".profile  #femi").effect("bounce", { times: 10, distance: -20 }, 1000);
-    }, 3000)
-
-}
-
-)
-
+    if ($(".body1").hasClass("dkmode")) {
+      $("#dkmodebutton").text("Switch to default");
+      $("#dkmodebutton").css("background-color", "#000000d2");
+      $("#dkmodebutton").css("color", "white");
+      $("#dkmodebutton").css("border", "1px solid white");
+    } else {
+      $("#dkmodebutton").text("Switch to darkmode");
+      $("#dkmodebutton").css("background-color", "#6a4e3083");
+    }
+  });
+});
